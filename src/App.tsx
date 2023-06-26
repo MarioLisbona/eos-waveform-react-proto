da7d4022a2d0181fa581b8e308e605b0488c3ae7
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-06-27 06:37:54 +1000

    Replace components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
 // import * as React from "react";
-import { Box, Text, Link, VStack, Code, Grid } from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
-import { Logo } from "./Logo";
+import { Flex, Text } from "@chakra-ui/react";
+import SectionContainer from "./components/SectionContainer";
+import WaveForm from "./components/waveform";
 
 export const App = () => (
-  <Box textAlign="center" fontSize="xl">
-    <Grid minH="100vh" p={3}>
-      <ColorModeSwitcher justifySelf="flex-end" />
-      <VStack spacing={8}>
-        <Logo h="40vmin" pointerEvents="none" />
-        <Text>
-          Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-        </Text>
-        <Link
-          color="teal.500"
-          href="https://chakra-ui.com"
-          fontSize="2xl"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Chakra
-        </Link>
-        <Text textStyle={"heading"}>This is some text</Text>
-      </VStack>
-    </Grid>
-  </Box>
+  <SectionContainer>
+    <Flex direction={"column"} align={"center"}>
+      <Text textStyle={"context"}>EOS Waveform Prototype</Text>
+      <WaveForm />
+    </Flex>
+  </SectionContainer>
 );
