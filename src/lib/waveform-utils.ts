fc4e758efbda5cb3ad00d7fad149ea61dd658c09
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-06-29 14:31:05 +1000

    Create all update's segments in timeline

--- src/lib/waveform-utils.ts
+++ src/lib/waveform-utils.ts
@@ -326,7 +326,13 @@ export const deleteAllSegments = (
   peaks.segments.removeAll();
   setSegments([]);
 };
+//////////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////////////////////
+//
+//             Create all segments
+//
+//
 // console logs the segments after all edits
 //set segments to empty array and destroy peaks instance to free resources
 export const createAllSegments = (
@@ -334,7 +340,25 @@ export const createAllSegments = (
   setSegments: React.Dispatch<React.SetStateAction<TestSegmentProps[]>>,
   segments: TestSegmentProps[]
 ) => {
-  console.log("Exporting clip data and destroying Peaks instance", segments);
+  const updatedSegments = segments.map((seg) => {
+    return {
+      ...seg,
+      formErrors: {
+        fileNameError: seg.formErrors.fileNameError,
+        startTimeError: false,
+        endTimeError: false,
+        isCreated: true,
+      },
+      editable: false,
+      color: "#384115",
+    };
+  });
+
+  setSegments(updatedSegments);
+  console.log(
+    "Exporting clip data and destroying Peaks instance",
+    updatedSegments
+  );
   // setSegments([]);
   // peaks?.destroy();
 };
