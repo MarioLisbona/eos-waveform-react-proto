2d4fc74f01cada254d30752a8247206531be1d17
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-06-30 09:33:51 +1000

    Top and Tail swapping for first two clips

--- src/lib/waveform-utils.ts
+++ src/lib/waveform-utils.ts
@@ -166,6 +166,37 @@ export const handleAddSegment = (
     //move the playhead to the start of the new segment
     myPeaks.player.seek(newSegment.startTime);
   } else if (
+    secondClip &&
+    !invalidPlayheadPosition &&
+    playheadPosition < segments[0].startTime
+  ) {
+    //swapping Top and tail clips if second clip added is before original Top clip
+    segments[0].fileName = "Tail";
+    segments[0].labelText = "Tail";
+
+    const newSegment = {
+      id: segments.length.toString(),
+      fileName: "Top",
+      startTime: playheadPosition,
+      endTime: playheadPosition + mediaLength * 0.03,
+      editable: true,
+      color: "#1E1541",
+      labelText: "Top",
+      formErrors: {
+        fileNameError: false,
+        startTimeError: false,
+        endTimeError: false,
+        isCreated: false,
+      },
+    };
+
+    //add new segment to the segments array, sort it by start time and update segments state
+    const updatedSegments = [...segments, newSegment];
+    setSegments(updatedSegments.sort((a, b) => a.startTime - b.startTime));
+
+    //move the playhead to the start of the new segment
+    myPeaks.player.seek(newSegment.startTime);
+  } else if (
     !invalidPlayheadPosition &&
     validGapLength !== -1
     // playheadPosition > segments[0].startTime &&
@@ -188,7 +219,7 @@ export const handleAddSegment = (
       },
     };
 
-    //add new segment to the segments array, sort it by sart time and update segments state
+    //add new segment to the segments array, sort it by start time and update segments state
     const updatedSegments = [...segments, newSegment];
     setSegments(updatedSegments.sort((a, b) => a.startTime - b.startTime));
 
