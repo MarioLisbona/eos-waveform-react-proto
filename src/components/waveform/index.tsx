2f15f14d6b9c680960c0ed48e98452d692dd0f88
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-06-29 17:46:49 +1000

    test conditional message in Modal

--- src/components/waveform/index.tsx
+++ src/components/waveform/index.tsx
@@ -173,22 +173,28 @@ export default function WaveForm() {
     };
   }, [myPeaks, handleClipDragEnd, handleZoomviewDblClick]);
 
+  const overlap = true;
   return (
     <>
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader>Unable to Add Segment</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Text textStyle={"context"}>Invalid playhead position</Text>
+            <Text textStyle={"smContext"}></Text>
+            {overlap
+              ? `Clips must be greater than ${(
+                  myPeaks?.player.getDuration()! * 0.03
+                ).toFixed(1)} seconds`
+              : "Clips must not overlap"}
+            {/* 'Please choose a gap greater than {(myPeaks?.player.getDuration()! * 0.03).toFixed(1)} seconds */}
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
+            <Button variant={"brandPrimaryMobileNav"} mr={3} onClick={onClose}>
               Close
             </Button>
-            <Button variant="ghost">Secondary Action</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
