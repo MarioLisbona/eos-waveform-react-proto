a40bca82de9ac03bd3709ad0cbfeca1146587a46
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-07-04 16:16:24 +1000

    Remove commented out modal from index

--- src/components/waveform/index.tsx
+++ src/components/waveform/index.tsx
@@ -190,27 +190,6 @@ export default function WaveForm() {
 
   return (
     <>
-      {/* <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Unable to Add Segment</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Text textStyle={"smContext"}></Text>
-            {clipOverlap
-              ? `There is not enough room for your clip. Please choose a gap larger than ${(
-                  myPeaks?.player.getDuration()! * 0.03
-                ).toFixed(1)} seconds`
-              : "A clip already exists at that position, clips cannot overlap. Please choose an empty gap on the timeline"}
-          </ModalBody>
-
-          <ModalFooter>
-            <Button variant={"brandPrimaryMobileNav"} mr={3} onClick={onClose}>
-              Close
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal> */}
       <InvalidTCPositionModal
         isOpen={isOpen}
         onClose={onClose}
