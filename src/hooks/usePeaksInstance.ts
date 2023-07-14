5f88a4ad8362b875ea377477f9d1e73b1dc48d04
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-07-14 12:49:28 +1000

    Remove unused AudioDataProps import

--- src/hooks/usePeaksInstance.ts
+++ src/hooks/usePeaksInstance.ts
@@ -1,6 +1,6 @@
 import { useState, useCallback, useEffect } from "react";
 import Peaks, { PeaksInstance, PeaksOptions } from "peaks.js";
-import { AudioDataProps, TestSegmentProps } from "../types";
+import { TestSegmentProps } from "../types";
 import { audioData, testSegmentsSmall } from "../data/segmentData";
 import {
   setPeaksConfig,
