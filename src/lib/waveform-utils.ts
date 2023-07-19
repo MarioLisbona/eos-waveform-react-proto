1320c78efd804005fae821a76fe0bd722f7fb855
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-07-19 15:04:41 +1000

    Create waveform-utils.ts

---
+++ src/lib/waveform-utils.ts
@@ -0,0 +1,35 @@
+import { PeaksInstance, SegmentDragEvent } from "peaks.js";
+import { TestSegmentProps } from "../types";
+import { ChangeEvent } from "react";
+import { createNewSegment } from "./general-utils";
+
+//////////////////////////////////////////////////////////////////////
+//
+//
+//              Create a generic top and tail segment covering
+//              90 % of the media duration
+//
+//
+export const createGenericTopTail = (
+  myPeaks: PeaksInstance,
+  segments: TestSegmentProps[],
+  setSegments: React.Dispatch<React.SetStateAction<TestSegmentProps[]>>
+) => {
+  const mediaLength = myPeaks.player.getDuration();
+  const playheadPosition = myPeaks.player.getCurrentTime();
+  const topTailSegment = {
+    id: "Top-n-Tail-Segment",
+    fileName: "Top-n-Tail-Segment",
+    startTime: playheadPosition,
+    endTime: mediaLength * 0.95,
+    editable: true,
+    color: "#1E1541",
+    labelText: "Top-n-Tail-Segment",
+    formErrors: {
+      fileNameError: false,
+      isCreated: false,
+    },
+  };
+
+  setSegments([topTailSegment]);
+};
